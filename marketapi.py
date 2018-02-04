4a13d14e819837a1d0cf9bd915cc70471286218f
Dakota Sanchez <dakota@dakotasanchez.com>
2018-02-03 17:34:10 -0800

    Create 2 endpoints, add distance filter
    Create markets endpoint for getting market info with a distance
    contraint. Requires an y (latitude) parameter and x (longitude)
    parameter. Also needs a dist parameter.
    
    Create market endpoint with an id parameter. Id is returned by the first
    endpoint.
    
    Also return distance with market info.

--- marketapi.py
+++ marketapi.py
@@ -1,11 +1,11 @@
-#import os
-#from flask import Flask, request, session, g, redirect, url_for, abort, jsonify
-from flask import Flask
+from flask import Flask, jsonify, request
+from sqlalchemy import inspect
 from flask_sqlalchemy import SQLAlchemy
 from models import Market
+from operator import itemgetter
+import math
 
 import simplejson as json
-from flask import jsonify
 
 """ CONFIG """
 
@@ -16,31 +16,77 @@ db = SQLAlchemy(app)
 
 """ VIEWS """
 
-@app.route('/markets')
-def hey():
-    data = db.session.query(Market.market_name,
-                            Market.street,
-                            Market.city,
-                            Market.state,
+@app.route('/api/v1/markets')
+def markets():
+    x = request.args.get('x')
+    y = request.args.get('y')
+    dist = request.args.get('dist')
+
+    """ Spherical Law of Cosines Formula """
+
+    clause = """3959 * acos(cos(radians(:lat)) * cos(radians(y)) * cos(radians(x)
+    - radians(:long)) + sin(radians(:lat)) * sin(radians(y))) < :dist"""
+
+    query = db.session.query(Market.fmid,
+                            Market.market_name,
+                            Market.website,
+                            Market.x,
+                            Market.y).filter(clause).params(lat=y, long=x, dist=dist)
+
+    results = [u._asdict() for u in query.all()]
+
+    for row in results:
+        row['distance'] = distance(x, y, row['x'], row['y'])
+
+    sorted_results = sorted(results, key=itemgetter('distance'))
+
+    return jsonify(markets = sorted_results)
+
+def object_as_dict(obj):
+    return {c.key: getattr(obj, c.key)
+            for c in inspect(obj).mapper.column_attrs}
+
+@app.route('/api/v1/market')
+def market():
+    fmid = request.args.get('id')
+
+    data = db.session.query(Market.city,
+                            Market.credit,
+                            Market.facebook,
+                            Market.fmid,
                             Market.location,
-                            Market.soap).filter(Market.city == 'Portland').all()
-    return jsonify(markets = data)
-    """return jsonify(markets=[d.json() for d in data])"""
+                            Market.x,
+                            Market.y,
+                            Market.market_name,
+                            Market.organic,
+                            Market.other_media,
+                            Market.season_1_date,
+                            Market.season_1_time,
+                            Market.sfmnp,
+                            Market.snap,
+                            Market.state,
+                            Market.street,
+                            Market.twitter,
+                            Market.update_time,
+                            Market.website,
+                            Market.wic,
+                            Market.wic_cash,
+                            Market.zipcode).filter(Market.fmid == fmid).first()
+    return jsonify(market = data)
 
-""" MISC """
 
-"""
-def haversine(lat1, long1, lat2, long2):
+def distance(my_long, my_lat, market_long, market_lat):
+    my_long = float(my_long)
+    my_lat = float(my_lat)
+    market_long = float(market_long)
+    market_lat = float(market_lat)
 
-#define d2r (M_PI / 180.0)
+    return 3959 * math.acos(math.cos(math.radians(my_lat)) \
+            * math.cos(math.radians(market_lat)) \
+            * math.cos(math.radians(market_long) \
+            - math.radians(my_long)) + math.sin(math.radians(my_lat)) \
+            * math.sin(math.radians(market_lat)))
 
-double dlong = (long2 - long1) * d2r;
-double dlat = (lat2 - lat1) * d2r;
-double a = pow(sin(dlat/2.0), 2) + cos(lat1*d2r) * cos(lat2*d2r) * pow(sin(dlong/2.0), 2);
-double c = 2 * atan2(sqrt(a), sqrt(1-a));
-double d = 3956 * c; 
-return d;
-"""
 
 """ MAIN """
 
